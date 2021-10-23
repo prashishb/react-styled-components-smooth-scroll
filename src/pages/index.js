c5c6086a33346e5def238acb6b50ea1f87f9bcc3
prashishb <prashb.web@gmail.com>
2021-10-23 02:22:25 -0400

    info / services section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import {
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree,
+} from '../components/InfoSection/Data';
 import Navbar from '../components/Navbar';
+import Services from '../components/Services';
 import Sidebar from '../components/Sidebar';
 
 const Home = () => {
@@ -15,6 +22,10 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Services />
+      <InfoSection {...homeObjThree} />
     </>
   );
 };
